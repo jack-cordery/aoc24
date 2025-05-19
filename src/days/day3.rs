507fc584b9788b06f38080f4d99d0a51cf4d8b17
Jack Cordery <jack.redolf@gmail.com>
2025-05-19 08:04:29 +0100

    day 3 part 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -17,6 +17,10 @@ pub fn day_three(path: &str) -> Result<()> {
     }
     let t = now.elapsed().as_micros();
     println!("total sum is {} in {}us ", total, t);
+    let input: String = contents.lines().collect();
+    let s = sum_file(&input);
+    let t = now.elapsed().as_micros();
+    println!("total sum of two is {} in {}us ", s, t);
     Ok(())
 }
 
@@ -24,6 +28,34 @@ fn sum_line(input: &str) -> u64 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let matches: Vec<u64> = re
         .find_iter(input)
+        .map(|m| re.captures(m.as_str()).unwrap())
+        .map(|c| {
+            let first = c.get(1).unwrap().as_str().parse::<u64>().unwrap();
+            let second = c.get(2).unwrap().as_str().parse::<u64>().unwrap();
+            first * second
+        })
+        .collect();
+    matches.iter().sum()
+}
+
+/// this time need to ensure to only take strings inbetween do() and don't()
+/// ensuring to check start and end
+/// ok so the program starts with a do() - so i could just append that on the front
+/// and add dont() onto the end but that has to be done on the input level -> just feed the whole
+/// string in not line by line then i can do it on this level
+fn sum_file(input: &str) -> u64 {
+    let input_corrected = format!("do() {} don't()", input);
+    let filter_re = Regex::new(r"do\(\)(.+?)don\'t\(\)").unwrap();
+    let input_corrected: String = filter_re
+        .captures_iter(&input_corrected)
+        .map(|c| {
+            let (_, [capt]) = c.extract();
+            capt
+        })
+        .collect();
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let matches: Vec<u64> = re
+        .find_iter(&input_corrected)
         .map(|m| re.captures(m.as_str()).unwrap())
         .map(|c| {
             let first = c.get(1).unwrap().as_str().parse::<u64>().unwrap();
